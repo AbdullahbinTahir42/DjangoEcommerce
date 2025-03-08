4cf0dbd1b9ea2d267fbfececf8c344ea4bd07123
AbdullahbinTahir42 <abdullahbintahir42@gmail.com>
2025-03-08 15:49:31 +0500

    cart,checkout pages dynamic

--- Ecommerce/store/models.py
+++ Ecommerce/store/models.py
@@ -37,6 +37,19 @@ class Order(models.Model):
     def __str__(self):
         return str(self.id)
     
+    @property
+    def get_cart_total(self):
+        orderitems = self.orderitems_set.all()
+        total = sum([item.get_total for item in orderitems])
+        return total
+    
+    
+    @property
+    def get_cart_items(self):
+        orderitems = self.orderitems_set.all()
+        total = sum([item.quantity for item in orderitems])
+        return total
+
 class OrderItems(models.Model):
     product = models.ForeignKey(Product,on_delete=models.SET_NULL,null=True,blank=True)
     order = models.ForeignKey(Order,on_delete=models.SET_NULL,null=True,blank=True)
