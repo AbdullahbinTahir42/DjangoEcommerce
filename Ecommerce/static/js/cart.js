cc3031423c2bdebc2f0953b11999b38967108c52
AbdullahbinTahir42 <abdullahbintahir42@gmail.com>
2025-03-20 14:56:50 +0500

    add to cart done

--- Ecommerce/static/js/cart.js
+++ Ecommerce/static/js/cart.js
@@ -1,43 +1,41 @@
-var updateBtns = document.getElementsByClassName('update-cart')
+var updateBtns = document.getElementsByClassName("update-cart");
 for (var i = 0; i < updateBtns.length; i++) {
-    updateBtns[i].addEventListener("click", function () {
-        var productID = this.dataset.product;
-        var action = this.dataset.action;
+  updateBtns[i].addEventListener("click", function () {
+    var productID = this.dataset.product;
+    var action = this.dataset.action;
 
-        console.log("productID:", productID, "Action:", action);
-        console.log("USER:", user); // Debugging
+    console.log("productID:", productID, "Action:", action);
+    console.log("USER:", user); // Debugging
 
-        if (user == "AnonymousUser") {
-            console.log("User is not logged in.");
-        } else {
-            updateUserOrder(productID,action)
-        }
-    });
+    if (user == "AnonymousUser") {
+      console.log("User is not logged in.");
+    } else {
+      updateUserOrder(productID, action);
+    }
+  });
 }
 
+function updateUserOrder(productID, action) {
+  console.log("User is logged in , sending data....");
 
-function updateUserOrder(productID,action)
-{
-    console.log("User is logged in , sending data....")
-
-    var url = '/update_item/'
-    fetch(url, {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-          'X-CSRFToken' : csrftoken
-        },
-        body:JSON.stringify({
-            'productID':productID,
-            'Action':action 
-        })
-})
- .then((response)=>{
-    return response.json()
- })
-    
- .then((data)=>{
-     console.log('Data : ', data)
- })
-
+  var url = "/update_item/";
+  fetch(url, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "X-CSRFToken": csrftoken,
+    },
+    body: JSON.stringify({
+      productID: productID,
+      Action: action,
+    }),
+  })
+    .then((response) => {
+      console.log("Response status:", response.status);
+      return response.json();
+    })
+    .then((data) => {
+      console.log("Data received:", data);
+      window.location.reload();
+    });
 }
--- Ecommerce/store/views.py
+++ Ecommerce/store/views.py
@@ -6,8 +6,17 @@ import json
 # Create your views here.
 
 def store(request):
+    if request.user.is_authenticated:
+        customer = request.user.customer
+        order , created = Order.objects.get_or_create(customer = customer,complete = False)
+        items = order.orderitems_set.all()
+        cart_items = order.get_cart_items
+    else:
+        items = []
+        order = {'get_cart_items': 0, 'get_cart_total' : 0}
+        cart_items = order['get_cart_items']
     products = Product.objects.all()
-    context = {"products" : products}
+    context = {"products" : products,'cart_items':cart_items}
     return render(request,'store/store.html',context)
 
 def cart(request):
@@ -15,11 +24,13 @@ def cart(request):
         customer = request.user.customer
         order , created = Order.objects.get_or_create(customer = customer,complete = False)
         items = order.orderitems_set.all()
+        cart_items = order.get_cart_items
     else:
         items = []
         order = {'get_cart_items': 0, 'get_cart_total' : 0}
+        cart_items = order['get_cart_items']
     
-    context = {'items' : items,'order' : order}
+    context = {'items' : items,'order' : order,'cart_items':cart_items}
     return render(request,'store/cart.html',context)
 
 def checkout(request):
@@ -40,7 +51,22 @@ def updateItem(request):
     productID = data['productID']
     action = data['Action']
 
-    print('Product ID : ' ,productID)
-    print('Action : ' ,action)
-    
-    return JsonResponse("Item was added" ,safe=False)
+    print('Product ID:', productID)
+    print('Action:', action)
+
+    customer = request.user.customer
+    product = Product.objects.get(id=productID)
+    order, created = Order.objects.get_or_create(customer=customer, complete=False)
+    orderitem, created = OrderItems.objects.get_or_create(order=order, product=product)
+
+    if action == 'add':
+        orderitem.quantity += 1
+    elif action == 'remove':     
+        orderitem.quantity -= 1 
+
+    orderitem.save()
+
+    if orderitem.quantity <= 0:
+        orderitem.delete()
+
+    return JsonResponse({"success": True}) 
